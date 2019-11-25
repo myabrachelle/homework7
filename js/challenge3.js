ba8da936ecad8e1aeb26280dd6133d4847fa3d6e
myabrachelle <37348933+myabrachelle@users.noreply.github.com>
2019-11-25 15:49:10 -0500

    Add files via upload

--- js/challenge3.js
+++ js/challenge3.js
@@ -1,50 +1,27 @@
 // In Challenge 3, this time make sure that at least 
 // one element from both sets of radio buttons is selected.
 
-$("#ch3form").click(function(){
-    var fruit = $('input[name="fruit"]')
-    var classStanding = $('input[name="standing"]')
-    
-    fruit.each(function(){
-        console.log("1st level");
-        if (this.is("checked")) {
-            console.log("2nd level");
-            return true;
-        }     
-    })
+$("#ch3form").submit(function(){
 
-    classStanding.each(function(){
-        if (this.is("checked")){
-            return true;
-        }
-    })
+    var fruitchecking = $('input[name=fruit]:checked')
+    var standingchecking = $('input[name=standing]:checked')
+    console.log(fruitchecking.val())
+
+    if(fruitchecking.val() == undefined && standingchecking.val() == undefined){
+        console.log(fruitchecking.val())
+        alert("You must pick a class standing or a fruit!")
+        return false;
+    }
+    else if (fruitchecking.val() == undefined){
+        alert("You must pick a fruit!")
+        return false;
+    }
+    else if (standingchecking.val() == undefined){
+        alert("You must pick a class standing!")
+        return false;
+    }
+    else{
+        return true;
+    }
     
-    alert("You must pick a class standing or a fruit!")
-    return false;
 });
-
-
-
-// ch3form.onsubmit = function() {
-//     var fruit = 
-//     document.querySelectorAll('input[name="fruit"]')
-
-
-//     for (let i = 0; i < fruit.length;i++){
-//         if (fruit[i].checked)
-//             return true;
-//     }
-
-//     alert("You must pick a fruit!")
-//     return false;
-// };
-
-
-
-    // Advanced Answer
-    // if(document.querySelector('input[name="fruit"]:checked'))
-    //     return true;
-    
-    // alert("You must pick a fruit!")
-    // return false;
-//   }
